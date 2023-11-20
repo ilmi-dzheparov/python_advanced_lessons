a4e8be20f07b29a9215619bf8c99e0cf1b3f0987
ilmi <jilmi@yandex.ru>
2023-11-20 20:02:25 +0100

    lesson 1

--- module_01_flask/homework/app.py
+++ module_01_flask/homework/app.py
@@ -1,42 +1,63 @@
 import datetime
+import os
+import random
+import re
+
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/hello_world')
-def test_function():
-    pass
+def hello_world():
+    return 'Привет, мир!'
 
 
+cars_list =['Chevrolet', 'Renault', 'Ford', 'Lada']
 @app.route('/cars')
-def test_function():
-    pass
+def cars_print():
+    return ', '.join(cars_list)
 
 
+cats_list = ['корниш-рекс', 'русская голубая', 'шотландская вислоухая', 'мейн-кун', 'манчкин']
 @app.route('/cats')
-def test_function():
-    pass
+def cats_choice():
+    return random.choice(cats_list)
+
 
 
 @app.route('/get_time/now')
-def test_function():
-    pass
+def get_time_now():
+    current_time = datetime.datetime.now().time()
+    return f"Точное время {current_time}"
+
 
 
 @app.route('/get_time/future')
-def test_function():
-    pass
+def get_time_en_hour():
+    current_time_after_hour = (datetime.datetime.now() + datetime.timedelta(hours=1)).time()
+    return f'Точное время через час будет {current_time_after_hour}'
 
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BOOK_FILE = os.path.join(BASE_DIR, 'war_and_peace.txt')
+
+with open(BOOK_FILE) as book:
+    book_content = book.read()
+
+book_content_words = re.findall(r'\w+', book_content)
 
 @app.route('/get_random_word')
-def test_function():
-    pass
+def random_word():
+    return random.choice(book_content_words)
 
 
+count = 0
 @app.route('/counter')
-def test_function():
-    pass
+def counter_function():
+    global count
+    count += 1
+    return f'Страница открылась {count} раз'
 
 
 if __name__ == '__main__':
--- module_01_flask/materials/first_application/app.py
+++ module_01_flask/materials/first_application/app.py
@@ -8,3 +8,16 @@ app = Flask(__name__)
 def test_function():
     now = datetime.datetime.now().utcnow()
     return f'Это тестовая страничка, ответ сгенерирован в {now}'
+
+
+@app.route('/hello')
+def hell0_world():
+    return 'Hello World!'
+
+
+count = 0
+@app.route('/count')
+def counter():
+    global count
+    count += 1
+    return f'Это тестовая страничка, ответ сгенерирован {count}'
