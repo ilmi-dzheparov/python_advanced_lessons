dd58199ecb0ffbfdf02704a059f3490ca1132285
ilmi <jilmi@yandex.ru>
2024-01-11 10:07:49 +0100

    Homework 6.4

--- module_06_debugging_begin/homework/hw3/json_adapter.py
+++ module_06_debugging_begin/homework/hw3/json_adapter.py
@@ -24,19 +24,37 @@ logger.info('Сообщение')
 
 Вам нужно дописать метод process так, чтобы в логах была всегда JSON-валидная строка.
 """
-
+import json
 import logging
+import re
+from datetime import datetime
 
 
 class JsonAdapter(logging.LoggerAdapter):
+
+
     def process(self, msg, kwargs):
-        new_message = msg
-        return new_message, kwargs
+        print(kwargs)
+        time = datetime.now().time().strftime("%H:%M:%S")
+        level= kwargs.pop('lev', self.extra['lev'])
+        print(level)
+        if len(re.findall(r'\"', msg)) % 2 == 1:
+            msg += '"'
+        msg = {"time": str(time), "level": level, "message": msg}
+        msg = json.dumps(msg, ensure_ascii=False)
+
+        return msg, kwargs
+
+
 
 
 if __name__ == '__main__':
-    logger = JsonAdapter(logging.getLogger(__name__))
+    logger = JsonAdapter(logging.getLogger(__name__), {"lev": "UNKNOWN"})
     logger.setLevel(logging.DEBUG)
-    logger.info('Сообщение')
-    logger.error('Кавычка)"')
-    logger.debug("Еще одно сообщение")
+    logging.basicConfig(filename='skillbox_json_messages.log',
+                        level=logging.DEBUG,
+                        format='%(message)s')
+    logger.info("Cообщение", lev="INFO")
+    logger.error('"Кавычка)"', lev="ERROR")
+    logger.debug("Еще одно сообщение", lev="DEBUG")
+
--- module_06_debugging_begin/homework/hw4/main.py
+++ module_06_debugging_begin/homework/hw4/main.py
@@ -8,7 +8,10 @@
 4. Сколько сообщений содержит слово dog.
 5. Какое слово чаще всего встречалось в сообщениях уровня WARNING.
 """
+import json
+import subprocess
 from typing import Dict
+from collections import Counter
 
 
 def task1() -> Dict[str, int]:
@@ -16,7 +19,18 @@ def task1() -> Dict[str, int]:
     1. Сколько было сообщений каждого уровня за сутки.
     @return: словарь вида {уровень: количество}
     """
-    pass
+    level_count = {
+        "DEBUG": 0,
+        "INFO": 0,
+        "ERROR": 0,
+        "CRITICAL": 0
+    }
+    for key in level_count.keys():
+        command = 'grep -c ' + "'" + '"level"' + ": " + f'"{key}"' + "'" + ' skillbox_json_messages.log'
+        res = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        level_count[key] = int(res.stdout)
+
+    return level_count
 
 
 def task2() -> int:
@@ -24,7 +38,22 @@ def task2() -> int:
     2. В какой час было больше всего логов.
     @return: час
     """
-    pass
+    hours_logs = []
+    count = 0
+    hour_max = 0
+    for hour in range(0, 24):
+        if hour < 10:
+            key_h = "0" + str(hour)
+        else:
+            key_h = str(hour)
+        command = 'grep ' + "'" + '"time"' + ": " + f'"{key_h}' + "'" + ' skillbox_json_messages.log'
+        res = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        lines = res.stdout.splitlines()
+        if count < len(lines):
+            count = len(lines)
+            hour_max = hour
+
+    return hour_max
 
 
 def task3() -> int:
@@ -32,7 +61,14 @@ def task3() -> int:
     3. Сколько логов уровня CRITICAL было в период с 05:00:00 по 05:20:00.
     @return: количество логов
     """
-    pass
+    count = 0
+    str_to_find = '"level"' + ": " + '"CRITICAL"'
+    for key_h in ("05:0", "05:1"):
+        command = 'grep ' + "'" + '"time"' + ": " + f'"{key_h}' + "'" + ' skillbox_json_messages.log'
+        res = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        count += res.stdout.count(str_to_find)
+
+    return count
 
 
 def task4() -> int:
@@ -40,7 +76,10 @@ def task4() -> int:
     4. Сколько сообщений содержат слово dog.
     @return: количество сообщений
     """
-    pass
+    command = 'grep -c ' + "'" + 'dog' + "'" + ' skillbox_json_messages.log'
+    res = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+    return int(res.stdout)
 
 
 def task5() -> str:
@@ -48,7 +87,20 @@ def task5() -> str:
     5. Какое слово чаще всего встречалось в сообщениях уровня WARNING.
     @return: слово
     """
-    pass
+    words = []
+    command = 'grep ' + "'" + '"level"' + ": " + f'"WARNING"' + "'" + ' skillbox_json_messages.log'
+    res = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    lines = res.stdout.splitlines()
+    for line in lines:
+        line_json = json.loads(line)
+        words.extend(line_json["message"].lower().split())
+    counter = Counter(words)
+    most_common_word = max(counter, key=counter.get)
+    return(most_common_word)
+
+
+
+
 
 
 if __name__ == '__main__':
