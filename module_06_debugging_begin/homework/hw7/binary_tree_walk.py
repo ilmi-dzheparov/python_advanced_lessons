c53a7a09c1b6be415455fbc5d45ece9da94b27ae
ilmi <jilmi@yandex.ru>
2024-01-14 21:50:04 +0100

    Homework 8

--- module_06_debugging_begin/homework/hw7/binary_tree_walk.py
+++ module_06_debugging_begin/homework/hw7/binary_tree_walk.py
@@ -20,7 +20,8 @@ import logging
 import random
 from collections import deque
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, List
+import re
 
 logger = logging.getLogger("tree_walk")
 
@@ -71,15 +72,49 @@ def get_tree(max_depth: int, level: int = 1) -> Optional[BinaryTreeNode]:
 
 
 def restore_tree(path_to_log_file: str) -> BinaryTreeNode:
-    pass
+    def find_node(line: str) -> List[str]:
+        rule = r'\[([^\]]+)\]'
+        pattern = re.compile(rule)
+        return pattern.findall(line)
+
+    nodes_dict = dict()
+    with open(path_to_log_file, 'r') as file:
+        lines = file.readlines()
+
+    for i in range(len(lines) - 1 , -1, -1):
+
+        if lines[i].split(":")[0] == "DEBUG":
+            nodes_in_line = find_node(lines[i])
+            node_father = int(nodes_in_line[0])
+            node_child: int = int(nodes_in_line[1])
+            if node_father not in nodes_dict:
+                nodes_dict[node_father] = {"value": None, "left": None, "right": None}
+            if lines[i].split()[1] == 'left':
+                nodes_dict[node_father]["left"] = nodes_dict[node_child]["value"]
+            if lines[i].split()[1] == 'right':
+                nodes_dict[node_father]["right"] = nodes_dict[node_child]["value"]
+        if lines[i].split(":")[0] == "INFO":
+            node = int(find_node(lines[i])[0])
+            if node not in nodes_dict:
+                nodes_dict[node] = {"value": BinaryTreeNode(val=node, left=None, right=None), "left": None, "right": None}
+            else:
+                nodes_dict[node]["value"] = BinaryTreeNode(val=node, left=nodes_dict[node]["left"], right=nodes_dict[node]["right"])
+
+    return nodes_dict[node]["value"]
+
 
 
 if __name__ == "__main__":
     logging.basicConfig(
         level=logging.DEBUG,
         format="%(levelname)s:%(message)s",
-        filename="walk_log_4.txt",
+        filename="walk_log_5.txt",
+        filemode='w'
     )
 
-    root = get_tree(7)
-    walk(root)
+    root = get_tree(3, 1)
+    # walk(root)
+    # restore_tree("walk_log_1.txt")
+    root_2 = restore_tree("walk_log_3.txt")
+    print(root_2)
+    walk(root_2)
--- module_06_debugging_begin/homework/hw8/my_t9.py
+++ module_06_debugging_begin/homework/hw8/my_t9.py
@@ -17,12 +17,44 @@
 Напишите функцию my_t9, которая принимает на вход строку, состоящую из цифр 2–9,
 и возвращает список слов английского языка, которые можно получить из этой последовательности цифр.
 """
+import re
 from typing import List
 
 
 def my_t9(input_numbers: str) -> List[str]:
-    ...
+    digit_letters = {
+        "2": "abc",
+        "3": "def",
+        "4": "ghi",
+        "5": "jkl",
+        "6": "mno",
+        "7": "pqrs",
+        "8": "tuv",
+        "9": "wxyz"
+    }
+    for sym in input_numbers:
+        if sym not in digit_letters.keys():
+            raise ValueError("Input should consist of digits 2-9 only")
 
+    with open("words.txt", "r") as file:
+        words_list: List[str] = file.readlines()
+
+    def search_words(words, letters, n) -> List[str]:
+        string = '.' * (n-1) + f'[{letters}].'
+        # print(string)
+        pattern = re.compile(f"^{string}", re.IGNORECASE)
+        matching_words = [word for word in words if pattern.match(word)]
+
+        return matching_words
+
+    new_words = words_list
+    n = 1
+    for sym in input_numbers:
+        letters = digit_letters[sym]
+        new_words = search_words(new_words, letters, n)
+        n += 1
+
+    return new_words
 
 if __name__ == '__main__':
     numbers: str = input()
