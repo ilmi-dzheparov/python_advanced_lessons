9de9dcb8987242eedf1dab914df54afb336a091b
ilmi <jilmi@yandex.ru>
2024-09-10 13:56:09 +0200

    hw_28_0

--- module_28_security/homework/task_2/hw.py
+++ module_28_security/homework/task_2/hw.py
@@ -1,12 +1,29 @@
-HTML = """
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-  {user_input}
-</body>
-</html>
-"""
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    html_content = """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+       <meta charset="UTF-8">
+       <title>Title</title>
+    </head>
+    <body>
+       <h1>Hello, World!</h1>
+       <script>
+         alert("Hacked!!!");
+       </script>
+    </body>
+    </html>
+    """
+    response = Response(html_content)
+    response.headers['Content-Security-Policy'] = "default-src 'self'; script-src 'self'; style-src 'self';"
+    # response.headers['X-XSS-Protection'] = '1; mode=block'
+    # response.headers['X-XSS-Protection'] = '0'
+    return response
+
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
